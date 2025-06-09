df0f90a794473c956054d3b3740fc6ced178fbe2
ahmedsm504 <ahmedsm504@gmail.com>
2025-06-09 05:07:16 -0700

    Update urls.py

--- superDoctor/urls.py
+++ superDoctor/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
+    path('' , include('groups.urls')),
     path('admin/', admin.site.urls),
      path('auth/', include('api_auth.urls')), 
      path('groups/' , include('groups.urls')),
