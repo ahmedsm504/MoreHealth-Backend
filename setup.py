affaad07121b79f2abac263c0fa8816f5f257370
ahmedsm504 <ahmedsm504@gmail.com>
2025-06-09 04:35:05 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+# setup.py في /home/ahmedsamir2025/MoreHealth-Backend/
+from setuptools import setup, find_packages
+
+setup(
+    name='MoreHealth',
+    version='0.1',
+    packages=find_packages(),
+)
